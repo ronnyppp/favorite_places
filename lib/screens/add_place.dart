50452d2f22ce25a4b6acd7bc0d432bf039e7ce6e
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-03-19 12:41:51 -0400

    first commit

---
+++ lib/screens/add_place.dart
@@ -0,0 +1,80 @@
+import 'dart:io';
+
+import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/widgets/image_input.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:favorite_places/providers/favorite_places.dart';
+import 'package:favorite_places/widgets/location_input.dart';
+
+class AddPlaceScreen extends ConsumerStatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  ConsumerState<AddPlaceScreen> createState() {
+    return _AddPlaceScreenState();
+  }
+}
+
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+  File? _selectedImage;
+  PlaceLocation? _selectedLocation;
+
+  void _savePlace() {
+    final enteredTitle = _titleController.text;
+
+    if(enteredTitle.isEmpty || _selectedImage == null || _selectedLocation == null) {
+      return;
+    }
+
+    ref.read(favoritePlacesProvider.notifier).addPlace(enteredTitle, _selectedImage!, _selectedLocation!);
+
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+      appBar: AppBar(title: const Text("Add new place"),),
+      body: 
+      SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              maxLength: 50,
+              decoration: const InputDecoration(label: Text("Place")),
+              controller: _titleController,
+              style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            const SizedBox(height: 10,),
+            ImageInput(onPickImage: (image) {
+              _selectedImage = image;
+            },),
+            const SizedBox(height: 10,),
+            LocationInput(
+              onSelectLocation: (location) {
+                _selectedLocation = location;
+              },
+            ),
+            const SizedBox(height: 16,),
+            Center(
+              child:
+            ElevatedButton.icon(onPressed: _savePlace,
+            icon: const Icon(Icons.add),
+            label: const Text("Add place"))
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,162 @@
+import 'dart:convert';
+
+import 'package:favorite_places/screens/map.dart';
+import 'package:flutter/material.dart';
+
+import 'package:favorite_places/models/place.dart';
+import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key, required this.onSelectLocation});
+
+  final void Function(PlaceLocation location) onSelectLocation;
+
+  @override
+  State<LocationInput> createState() {
+    return _LocationInputState();
+  }
+}
+
+class _LocationInputState extends State<LocationInput> {
+  PlaceLocation? _pickedLocation;
+  var _isGettingLocation = false;
+
+  String get locationImage {
+    if (_pickedLocation == null) {
+      return '';
+    }
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyCXVQvLPjQj101bslx2P9B1WznFWEX5Gnk';
+  }
+
+  Future<void> _savePlace(double latitude, double longitude) async {
+     final url = Uri.parse(
+      'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=AIzaSyCXVQvLPjQj101bslx2P9B1WznFWEX5Gnk',
+    );
+    final response = await http.get(url);
+    final respData = json.decode(response.body);
+    final address = respData['results'][0]['formatted_address'];
+
+    setState(() {
+      _pickedLocation = PlaceLocation(
+        latitude: latitude,
+        longitude: longitude,
+        address: address,
+      );
+      _isGettingLocation = false;
+    });
+
+    widget.onSelectLocation(_pickedLocation!);
+  }
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+
+    if (lat == null || lng == null) {
+      return;
+    }
+
+    _savePlace(lat, lng);
+  }
+
+  void _selectOnMap() async {
+    final pickedLocation = await Navigator.of(
+      context,
+    ).push<LatLng>(MaterialPageRoute(builder: (ctx) => const MapScreen(),
+    ),
+    );
+
+    if(pickedLocation == null) {
+      return;
+    }
+
+    _savePlace(pickedLocation.latitude, pickedLocation.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      "No location chosen",
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+        color: Theme.of(context).colorScheme.onSurface,
+      ),
+    );
+
+    if (_pickedLocation != null) {
+      previewContent = Image.network(
+        locationImage,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
+    }
+    if (_isGettingLocation) {
+      previewContent = const CircularProgressIndicator();
+    }
+
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Theme.of(
+                context,
+              ).colorScheme.primary.withValues(alpha: 0.2),
+            ),
+          ),
+          child: previewContent,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              icon: const Icon(Icons.location_on),
+              onPressed: _getCurrentLocation,
+              label: const Text("Get Current Location"),
+            ),
+            TextButton.icon(
+              icon: const Icon(Icons.map),
+              onPressed: _selectOnMap,
+              label: const Text("Select Map Location"),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
